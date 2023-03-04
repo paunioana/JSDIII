ba89b30807190762d8ebadf46e0345b2dfd03129
paunioana <58565952+Pioana01101@users.noreply.github.com>
2023-03-04 12:25:33 +0200

    12:25 Saturday

--- Mar01Homework/src/ro/MyAspect.java
+++ Mar01Homework/src/ro/MyAspect.java
@@ -1,6 +1,7 @@
 package ro;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -9,12 +10,11 @@ import java.util.Arrays;
 
 @Aspect
 public class MyAspect {
-    @Pointcut("execution(* run())")
+    @Pointcut("execution(* *run())")
     private void anyRunMethod() {}
 
-    @Before(value = "anyRunMethod", argNames = "joinPoint")
-    public void beforeCall(JoinPoint joinPoint) {
-        System.out.println("Method Name :" + joinPoint.getSignature().toShortString() +
-                "| Args => " + Arrays.asList(joinPoint.getArgs()));
+    @After(value = "anyRunMethod()", argNames = "joinPoint")
+    public void afterCall(JoinPoint joinPoint) {
+        System.out.println("Method Name: " + joinPoint.getSignature().toShortString());
     }
 }
